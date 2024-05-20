63c85cdd6f6fd86d2b256b029e4690b4ead2a375
ramilsaavedra <ramilsmcl@gmail.com>
2024-05-20 22:01:39 +0800

    feat: add nextui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Editor from '@/components/editor'
 
 export default function Home() {
 	return (
-		<main className="bg-gray-900 h-screen w-full">
-			<h1 className="text-center font-bold text-white py-3 text-2xl">Rich Text Editor</h1>
+		<main className="bg-gray-100 h-screen w-full">
+			<h1 className="text-center font-bold py-3 text-2xl">Rich Text Editor</h1>
 			<Editor />
 		</main>
 	)
